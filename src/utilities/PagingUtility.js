f42d4ff1d54fcc5e581cda560cc4de61baf84a59
Shamal Durbule <sdurbule24@gmail.com>
2022-08-01 15:50:53 +0530

    adminUi

---
+++ src/utilities/PagingUtility.js
@@ -0,0 +1,13 @@
+import config from '../personPerPage'
+
+
+export const getTotalPages = (length)=> {
+    return Math.ceil(length/10);
+}
+
+
+export const getRecordIndex = (page)=> {
+    return (page-1)*config.PAGE_SIZE ;
+}
+
+
